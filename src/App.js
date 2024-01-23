4b556254855aa6d48e61adb1d77a5608ad536b2d
sublime66 <terry61@btinternet.com>
2024-01-23 11:22:16 +0000

    Add initial view state and map style to DeckGL component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import DeckGL from "@deck.gl/react";
 import { Map } from "react-map-gl";
+
+const locations = {
+  london: {
+    longitude: -0.1275,
+    latitude: 51.5072,
+    zoom: 9,
+    pitch: 45,
+    bearing: 0,
+  },
+  paris: {
+    longitude: 2.3522,
+    latitude: 48.58572,
+    zoom: 10,
+    pitch: 45,
+    bearing: 0,
+  },
+};
+
 function App() {
+  const [view, setView] = useState(locations["london"]);
+
   return (
-    <DeckGL>
+    <DeckGL initialViewState={view} controller={true}>
       <Map
         mapStyle="mapbox://styles/mapbox/dark-v11"
         mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}

ea6bb17c0d3d7f7de967a967342400feb834b4a2
sublime66 <terry61@btinternet.com>
2024-02-07 10:32:33 +0000

    Adding control buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import DeckGL from "@deck.gl/react";
-import { PolygonLayer } from "@deck.gl/layers";
+import { GeoJsonLayer } from "@deck.gl/layers";
 import { Map } from "react-map-gl";
 import * as d3 from "d3";
+import Ajv from "ajv";
 
+import styles from "./App.module.css";
 import "mapbox-gl/dist/mapbox-gl.css";
+
+const ajv = new Ajv();
 
 const locations = {
   london: {
@@ -23,60 +27,133 @@ const locations = {
   },
 };
 
-const polygonData = [
-  {
-    contours: [
-      [-0.489, 51.28],
-      [-0.489, 51.78],
-      [0.236, 51.78],
-      [0.236, 51.28],
-      [-0.489, 51.28],
-    ],
-    height: 10000,
-    name: "London",
-    colour: "red",
-  },
-  {
-    contours: [
-      [-0.1, 52.0],
-      [-0.1, 52.5],
-      [0.5, 52.5],
-      [0.5, 52.0],
-      [-0.1, 52.0],
-    ],
-    height: 100000,
-    name: "Paris",
-    colour: [0, 255, 0],
-  },
-];
 function App() {
   const [view, setView] = useState(locations["london"]);
-  const [data, setData] = useState(polygonData);
+  const [populationData, setPopulationData] = useState(null);
+  const [years, setYears] = useState([]);
+  const [layerData, setLayerData] = useState([]);
+  const [combinedData, setCombinedData] = useState([]);
+  const [validationError, setValidationError] = useState(null);
+
+  const [currentYear, setCurrentYear] = useState([2000]);
+  const [heightObj, setHeightObj] = useState();
+  const [tooltip, setTooltip] = useState(null);
+
+  // const schema = {
+  //   type: "object",
+  //   properties: {
+  //     type: { type: "string" },
+  //     features: { type: "array" },
+  //   },
+  //   required: ["type", "features"],
+  // };
+
+  useEffect(() => {
+    let geojsonUrl = "https://skgrange.github.io/www/data/london_sport.json";
+    let csvUrl =
+      "https://data.london.gov.uk/download/land-area-and-population-density-ward-and-borough/77e9257d-ad9d-47aa-aeed-59a00741f301/housing-density-borough.csv";
+
+    Promise.all([d3.json(geojsonUrl), d3.csv(csvUrl)])
+      .then(([boroughs, populations]) => {
+        setLayerData(boroughs);
+
+        setPopulationData(populations);
+      })
+      .catch((error) => console.error("Error loading data:", error));
+  }, []);
+
+  useEffect(() => {
+    if (populationData) {
+      const uniqueYears = [...new Set(populationData.map((item) => item.Year))];
+      setYears(uniqueYears);
+    }
+  }, [populationData]);
+
+  useEffect(() => {
+    if (layerData && populationData) {
+      const currentYearData = populationData.filter(
+        (item) => item.Year === currentYear
+      );
+      const newCombinedData = layerData.features.map((feature) => {
+        const name = feature.properties.name;
+
+        const populationItem = populationData.find(
+          (data) => data.Name === name && data.Year === currentYear
+        );
+
+        if (populationItem) {
+          feature.properties.color = (255, 0, 0);
+          feature.properties.h = populationItem.population;
+        }
+
+        return feature;
+      });
+
+      // Set the new data object to combinedData state
+      setCombinedData(newCombinedData);
+      console.log(combinedData);
+    }
+  }, [layerData, populationData, currentYear]);
+
+  const validate = ajv.compile(schema);
+
+  useEffect(() => {
+    const valid = validate(geodata);
+    if (!valid) {
+      console.log(validate.errors);
+    } else {
+      console.log("yea");
+    }
+  }, [geodata]);
 
   const layers = [
-    new PolygonLayer({
-      id: "london-polygon",
-      data: polygonData,
-      stroked: true,
-      filled: true,
+    new GeoJsonLayer({
+      id: "geojson-layer",
+      data: geodata,
+      getFillColor: (d) => d.properties.color, // Set polygons to red
+
+      material: {
+        ambient: 0.64,
+        diffuse: 0.6,
+        shininess: 32,
+      },
       extruded: true,
-      wireframe: true,
-      lineWidthMinPixels: 1,
-      getPolygon: (d) => d.contours,
-      getElevation: (d) => d.height,
-      getLineColor: [250, 80, 80],
-      getFillColor: (d) => d.colour,
-      getLineWidth: 25,
+      pickable: true,
+      getElevation: (d) => d.properties.h,
+      onHover: (info) => setTooltip(info),
     }),
   ];
 
   return (
-    <DeckGL initialViewState={view} controller={true} layers={layers}>
-      <Map
-        mapStyle="mapbox://styles/mapbox/dark-v11"
-        mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-      />
-    </DeckGL>
+    <div className={styles.control}>
+      <select onChange={(e) => setCurrentYear(e.target.value)}>
+        {years.map((year) => (
+          <option key={year} value={year}>
+            {year}
+          </option>
+        ))}
+      </select>
+
+      <DeckGL initialViewState={view} controller={true} layers={layers}>
+        <Map
+          mapStyle="mapbox://styles/mapbox/dark-v11"
+          mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+        />
+        {tooltip && tooltip.object && (
+          <div
+            style={{
+              position: "absolute",
+              zIndex: 1,
+              pointerEvents: "none",
+              left: tooltip.x,
+              top: tooltip.y,
+            }}
+          >
+            {/* Display tooltip content here, e.g., tooltipInfo.object.properties.name */}
+          </div>
+        )}{" "}
+      </DeckGL>
+    </div>
   );
 }
 
